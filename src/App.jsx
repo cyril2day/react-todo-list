1e9c5f973bf35d12f45db79590457d77a0adb22e
psi <cy.lonsido@gmail.com>
2026-09-08 21:39:58 +0800

    render tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react"
 import TaskForm from "./components/TaskForm"
 import TaskControls from "./components/TaskControls"
 import TaskList from "./components/TaskList"
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Buy groceries',
+      priority: 1,
+      done: true
+    },
+    {
+      id: 2,
+      text: 'Have a walk',
+      priority: 2,
+      done: false
+    },
+    {
+      id: 3,
+      text: 'Read a book',
+      priority: 3,
+      done: false
+    }
+  ])
+
   return (
     <div
       style={{
@@ -15,7 +37,7 @@ const App = () => {
       <h2 style={{ textAlign: 'center' }}>To-Do List</h2>
       <TaskForm />
       <TaskControls />
-      <TaskList />
+      <TaskList tasks={tasks} />
     </div>
   )
 }
